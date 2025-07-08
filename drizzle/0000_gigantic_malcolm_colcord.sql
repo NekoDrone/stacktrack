39d3ef6672ed18b11dd1f44a2a833226b5934491
NekoDrone <sylfr.serenity.tan@gmail.com>
2025-07-08 14:22:51 +0800

    chore: run drizzle migration

---
+++ drizzle/0000_gigantic_malcolm_colcord.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `projects` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`status` text DEFAULT 'inactive' NOT NULL,
+	`votes` integer NOT NULL,
+	`is_priority` integer NOT NULL,
+	`created_at` integer DEFAULT (unixepoch('now')) NOT NULL
+);
+--> statement-breakpoint
+CREATE INDEX `idx_example_id` ON `projects` (`id`);
