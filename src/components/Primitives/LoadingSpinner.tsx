c52215ce9861d92039fa922c304d10db599f72be
NekoDrone <sylfr.serenity.tan@gmail.com>
2025-07-10 15:34:22 +0800

    feat: loading spinner component

---
+++ src/components/Primitives/LoadingSpinner.tsx
@@ -0,0 +1,5 @@
+import { SvgSpinners90Ring } from "@/components/Icons/SvgSpinners90Ring";
+
+export const LoadingSpinner = ({ className }: { className?: string }) => {
+    return <SvgSpinners90Ring className={className} />;
+};
