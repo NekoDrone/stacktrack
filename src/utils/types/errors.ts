7931fb56f5719a61c25eb148250dc2a7385dab77
NekoDrone <sylfr.serenity.tan@gmail.com>
2025-07-10 15:32:56 +0800

    feat: provide error types

---
+++ src/utils/types/errors.ts
@@ -0,0 +1,18 @@
+export interface ApiError {
+    type?: ErrorType;
+    message: string;
+    details?: unknown;
+}
+
+export enum GeneralErrorType {
+    TYPE_ERROR = "Type error.",
+    ENV_UNSET = "An environment variable was not properly set.",
+    PARAMS_ERROR = "Query params were set incorrectly.",
+    SERVER_ERROR = "Something went wrong on the server.",
+}
+
+export enum EntryErrorType {
+    ENTRY_DOES_NOT_EXIST = "Entry does not exist in db.",
+}
+
+export type ErrorType = GeneralErrorType | EntryErrorType;
