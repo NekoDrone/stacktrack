7c6ccdf04d25ad88783d32ebe4137933a29a07f0
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2025-07-18 13:04:00 +0800

    feat: additional label on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,9 @@ const IndexPage = () => {
         <div className="bg-ctp-base flex min-h-screen flex-col items-center justify-center gap-8">
             {isAdmin && <p>hey {eitherAdminGreeting ? "sexy" : "queen"}</p>}
             {isAdmin && <AddEntry />}
+            <div>
+                <p>Serenity is currently working on:</p>
+            </div>
             <EntriesList />
         </div>
     );
