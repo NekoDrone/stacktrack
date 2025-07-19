8f0a2d27943e569cb8d248195c1dd7ce5e136244
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2025-07-19 19:21:29 +0800

    refactor: better animated link handling

--- src/components/AnimatedUnderlineLink.tsx
+++ src/components/AnimatedUnderlineLink.tsx
@@ -1,37 +1,45 @@
 import { motion } from "motion/react";
-import { FC, ReactNode } from "react";
+import type { FC, ReactNode } from "react";
+
+enum UnderlineColours {
+    BLUE = "bg-ctp-blue",
+    MAUVE = "bg-ctp-mauve",
+    RED = "bg-ctp-red",
+}
 
 interface AnimatedLinkProps {
-  href: string;
-  children: ReactNode;
-  className: string;
+    href: string;
+    children: ReactNode;
+    className?: string;
+    underlineColour?: UnderlineColours | string;
 }
 
 export const AnimatedUnderlineLink: FC<AnimatedLinkProps> = ({
-  href,
-  children,
-  className,
+    href,
+    children,
+    className,
+    underlineColour = UnderlineColours.BLUE,
 }) => {
-  return (
-    <motion.a
-      href={href}
-      className={`relative inline-block ${className}`}
-      initial="initial"
-      whileHover="hover"
-    >
-      {children}
-      <motion.span
-        className="absolute bottom-1 left-0 w-full h-0.25 bg-ctp-blue origin-center"
-        variants={{
-          initial: { scaleX: 0 },
-          hover: { scaleX: 1 },
-        }}
-        transition={{
-          type: "spring",
-          duration: 0.25,
-          bounce: 0.3,
-        }}
-      />
-    </motion.a>
-  );
+    return (
+        <motion.a
+            href={href}
+            className={`relative inline-block ${className ?? ""}`}
+            initial="initial"
+            whileHover="hover"
+        >
+            {children}
+            <motion.span
+                className={`${underlineColour} absolute bottom-1 left-0 h-0.25 w-full origin-center`}
+                variants={{
+                    initial: { scaleX: 0 },
+                    hover: { scaleX: 1 },
+                }}
+                transition={{
+                    type: "spring",
+                    duration: 0.25,
+                    bounce: 0.3,
+                }}
+            />
+        </motion.a>
+    );
 };
