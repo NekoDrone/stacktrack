583ebf818202bb36fd8f4677c3d982bf5afd82f7
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2025-07-19 18:17:52 +0800

    feat: info icon

---
+++ src/components/Icons/LucideInfo.tsx
@@ -0,0 +1,25 @@
+import type { SVGProps } from "react";
+
+export const LucideInfo = (props: SVGProps<SVGSVGElement>) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+            {...props}
+        >
+            {/* Icon from Lucide by Lucide Contributors - https://github.com/lucide-icons/lucide/blob/main/LICENSE */}
+            <g
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+            >
+                <circle cx="12" cy="12" r="10" />
+                <path d="M12 16v-4m0-4h.01" />
+            </g>
+        </svg>
+    );
+};
